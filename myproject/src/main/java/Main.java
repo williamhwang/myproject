aa7ecbba9288b85cb7a56aac1652fd2b11b2efbb
William Hwang <william.hwang@lockeloard.com>
2016-03-29 13:32:31 -0500

    First try

---
+++ myproject/src/main/java/Main.java
@@ -0,0 +1,33 @@
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.*;
+
+public class Main {
+    
+    public static void main(String[] args) throws Exception {
+        
+        Connection connection = getConnection();
+        
+        Statement stmt = connection.createStatement();
+        stmt.executeUpdate("DROP TABLE IF EXISTS ticks");
+        stmt.executeUpdate("CREATE TABLE ticks (tick timestamp)");
+        stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+        boolean status = stmt.execute("select addfilm()");
+        ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+        while (rs.next()) {
+            System.out.println("Read from DB: " + rs.getTimestamp("tick") + " Executed");
+        }
+    }
+    
+    private static Connection getConnection() throws URISyntaxException, SQLException {
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + dbUri.getPath();
+
+        return DriverManager.getConnection(dbUrl, username, password);
+    }
+
+}
